737e979d72baa298ee6616b2ee66c451f79874a4
s01rice <s01rice@hotmail.ca>
2024-11-14 19:46:50 -0500

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://s01rice.github.io",
+  base: "hearthy",
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
+});
